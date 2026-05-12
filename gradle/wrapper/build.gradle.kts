8819ab3d669494fa54523cb9dcf585222f6022a2
arseniy1607-debug <arseniy160607@gmail.com>
2026-05-12 21:21:34 +0300

    Update build.gradle.kts

--- gradle/wrapper/build.gradle.kts
+++ gradle/wrapper/build.gradle.kts
@@ -1,6 +1,4 @@
-// Топ-левел файл конфігурації
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
