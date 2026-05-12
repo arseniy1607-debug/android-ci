ba0f71c5c6774134f83a757d47287b9a24ace8e9
arseniy1607-debug <arseniy160607@gmail.com>
2026-05-12 21:31:56 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.myapplication" // перевірте, щоб це співпадало з вашим пакетом
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
